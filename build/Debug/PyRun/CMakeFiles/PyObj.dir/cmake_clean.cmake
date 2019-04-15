38d76e921026b8c0a41036026d59fd0e35767954
PinkZhi <zywoffice@163.com>
2019-04-14 20:52:24 -0700

    add build paht

---
+++ build/Debug/PyRun/CMakeFiles/PyObj.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PyObj.dir/ExcutePyRun.cpp.o"
+  "CMakeFiles/PyObj.dir/HelloPython.cpp.o"
+  "CMakeFiles/PyObj.dir/PyObj.cpp.o"
+  "libPyObj.pdb"
+  "libPyObj.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PyObj.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
